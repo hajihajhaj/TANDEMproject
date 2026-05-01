7a7ed3f39d06a3f635eb68c604d8b8eab51c5d8c
hajihajhaj <163481952+hajihajhaj@users.noreply.github.com>
2026-05-01 12:43:56 -0400

    added bike bell (player 1 R2)

--- Assets/CODE/BikeBell.cs
+++ Assets/CODE/BikeBell.cs
@@ -1,24 +1,25 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BikeBell : MonoBehaviour
 {
-    public float bellRadius = 5f;
-    public LayerMask npcLayer;
     public AudioSource bellSound;
+
+    private Gamepad p1;
 
     void Update()
     {
-        if (Input.GetButtonDown("P1_R2"))
+        // Always lock Player 1 to first connected gamepad
+        if (p1 == null && Gamepad.all.Count > 0)
+            p1 = Gamepad.all[0];
+
+        if (p1 == null) return;
+
+        // Player 1 R2
+        if (p1.rightTrigger.wasPressedThisFrame)
         {
-            RingBell();
+            if (bellSound != null)
+                bellSound.Play();
         }
-    }
-
-    void RingBell()
-    {
-        if (bellSound != null)
-            bellSound.Play();
-
-    
     }
 }
